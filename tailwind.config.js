e8009f16d64b34a1d9407de96ee9fb0384480fee
developergokselglobal <developer@gokselglobal.com>
2023-12-19 21:47:19 +0300

    a

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    // Use prefix tw- to avoid conflicts with other Dawn styles
+    // prefix: 'tw-',
+    content: [
+        "./**/*.{js,json,liquid}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
